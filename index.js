ebf86f8b272a24ce6f23b6c33ac3fa01d9d7dd6c
sandeepdkl <43859080+sandeepdkl@users.noreply.github.com>
2023-07-19 00:43:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write(" Noofvariables=3 \n\
-              hello\
-              dear");
+hello\n\
+dear");
     
     res.end();
 }).listen(process.env.PORT || 3000);
